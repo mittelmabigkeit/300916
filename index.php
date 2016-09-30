1b4c2c29768e27c9d1e52c7f1dd931a1880e8b68
mittelmabigkeit <comp422012016@mail.ru>
2016-09-30 15:28:34 +0300

    privet

--- index.php
+++ index.php
@@ -18,4 +18,35 @@ echo $strtotime;
 echo "<br>";
 $getdate=getdate();
 print_r ($getdate);
+echo "<br>";
+if (checkdate(9, 30, 2016))
+{
+	echo "da";
+}
+else
+{
+	echo "net";
+}
+echo "<br>";
+//$phpinfo=phpinfo();
+//echo $phpinfo;
+//echo "<br>";
+$phpversion=phpversion();
+echo $phpversion;
+echo "<br>";
+function local2gm($localStamp=false) {
+	if ($localStamp === false) $localStamp = time();
+	$tzOffset=date("Z", $localStamp);
+	return $localStamp - $tzOffset;
+}
+function gm2local($gmStamp=false, $tzOffset=false)
+{
+	if ($gmStamp === false) return time();
+	if ($tzOffset === false)
+		$tzOffset=date("Z",$gmStamp);
+	else
+		$tzOffset*=60*60;
+	return $gmStamp+$tzOffset;
+}
+echo local2gm(time());
 ?>
